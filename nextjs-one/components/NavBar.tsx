d30b791a9d26434c779b2d9b79f7a23e82b37fd5
1eedaegon <d8726243@gmail.com>
2022-02-28 00:12:56 +0900

    Add css-in-js ugly

--- nextjs-one/components/NavBar.tsx
+++ nextjs-one/components/NavBar.tsx
@@ -1,17 +1,17 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import styles from './NavBar.module.css';
 
 const NavBar: NextPage = () => {
     const router = useRouter();
-    console.log(router);
     return (
     <nav>
         <Link href="/">
-            <a>Home</a>
+            <a className={router.pathname === '/' ? styles.active : ""}>Home</a>
         </Link>
         <Link href="/about">
-            <a>About</a>
+            <a className={router.pathname === '/about' ? styles.active : ""}>About</a>
         </Link>
     </nav>
     )
