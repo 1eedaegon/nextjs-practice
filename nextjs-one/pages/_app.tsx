16baffa08dee57df0a0c1c79b171e714f29f759e
1eedaegon <d8726243@gmail.com>
2022-02-28 01:23:10 +0900

    Add _app and global css

--- nextjs-one/pages/_app.tsx
+++ nextjs-one/pages/_app.tsx
@@ -2,7 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <div>
+    <Component {...pageProps} />
+    <span>global hello!</span>
+  </div>)
 }
 
 export default MyApp
--- nextjs-one/pages/index.tsx
+++ nextjs-one/pages/index.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import { useState } from 'react'
 import NavBar from '../components/NavBar'
-import styles from '../styles/Home.module.css'
+
 
 const Home: NextPage = () => {
   const [counter, setCounter] = useState(0)
