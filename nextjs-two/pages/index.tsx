0f5a845310bf20597466100572d471530e3be3c8
1eedaegon <d8726243@gmail.com>
2022-03-06 20:20:06 +0900

    Add sample tailwind component

--- nextjs-two/pages/index.tsx
+++ nextjs-two/pages/index.tsx
@@ -1,8 +1,28 @@
 import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black">it works</h1>
+    <div className="bg-slate-400 py-20 px-10 grid gap-5">
+      <div className="bg-white p-10 rounded-xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-3">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between my-3">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span className="">Total</span>
+          <span className="font-semibold">$19</span>
+        </div>
+        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">
+          <span>Checkout</span>
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-xl shadow-xl"></div>
     </div>
   );
 };
