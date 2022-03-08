f663b99d89078d2f5deaf08f8c7f3a10c8c6ddba
1eedaegon <d8726243@gmail.com>
2022-03-08 22:40:24 +0900

    Add sample component 3

--- nextjs-two/pages/index.tsx
+++ nextjs-two/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-5">
+    <div className="bg-slate-400 py-20 px-10 grid gap-5 min-h-screen">
       <div className="bg-white p-10 rounded-xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
         <div className="flex justify-between my-3">
@@ -16,33 +16,73 @@ const Home: NextPage = () => {
           <span className="">Total</span>
           <span className="font-semibold">$19</span>
         </div>
-        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">
+        <button
+          className="mt-5 bg-blue-500 text-white p-3 text-center
+         rounded-xl w-3/4 mx-auto
+         hover:bg-teal-500 hover:text-black
+         active:bg-yellow-500 focus:bg-red-500"
+        >
           <span>Checkout</span>
-        </div>
+        </button>
       </div>
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
         <div className="bg-blue-600 p-6 pb-14">
           <span className="text-white text-2xl">Profile</span>
         </div>
-        <div className="rounded-xl bg-white relative -top-14">
-          <div className="">
-            <div className="">
-              <span>Orders</span>
-              <span>340</span>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
             </div>
-            <div className="">프로필사진</div>
-            <div className="">
-              <span>Spent</span>
-              <span>340</span>
+            <div className="h-24 w-24 bg-red-400 rounded-full"></div>
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">340</span>
             </div>
           </div>
-          <div className="">
-            <span>Tony Molly</span>
-            <span>미국</span>
+          <div className="relative  flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Tony Molly</span>
+            <span className="text-sm text-gray-500">미국</span>
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-xl shadow-xl">
+        <div className="flex mb-5 justify-between items-center">
+          <span>⬅️</span>
+          <div className="space-x-3">
+            <span>⭐4.9</span>
+            <span className="shadow-xl p-2 rounded-md">💖</span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-72 mb-5" />
+        <div className="">
+          <span className="font-medium mb-1.5 text-xl">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex justify-between items-center">
+            <div>
+              <button className="w-5 h-5 rounded-full bg-yellow-500"></button>
+              <button className="w-5 h-5 rounded-full bg-indigo-500"></button>
+              <button className="w-5 h-5 rounded-full bg-teal-500"></button>
+            </div>
+            <div className="flex items-center space-x-4">
+              <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-around items-center">
+            <span className="font-medium text-2xl">$450</span>
+            <button className="bg-blue-500 py-2 px-8 text-center text-xs text-white rounded-lg">
+              Add to car
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-xl shadow-xl"></div>
     </div>
   );
