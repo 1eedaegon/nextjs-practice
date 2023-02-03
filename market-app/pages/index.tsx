e68edf14c27e1ae19bc158a027367b21e4d916f7
1eedaegon <d8726243@gmail.com>
2023-02-03 22:58:24 +0900

    Add style and develop modules

---
+++ market-app/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="space-y-4">
+      <div className="w-96 rounded bg-white shadow">w-96</div>
+      <div className="w-80 rounded bg-white shadow">w-80</div>
+      <div className="w-72 rounded bg-white shadow">w-72</div>
+      <div className="w-64 rounded bg-white shadow">w-64</div>
+      <div className="w-60 rounded bg-white shadow">w-60</div>
+      <div className="w-56 rounded bg-white shadow">w-56</div>
+      <div className="w-52 rounded bg-white shadow">w-52</div>
+      <div className="w-48 rounded bg-white shadow ">w-48</div>
+    </div>
+  );
+}
